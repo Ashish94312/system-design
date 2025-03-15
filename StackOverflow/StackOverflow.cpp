75f8557df3b964f124fdf65a6c761d8fb9595f42
ashish.kumar85297 <ashish.kumar85297@gmail.com>
2025-03-16 00:23:13 +0530

    .

--- StackOverflow/StackOverflow.cpp
+++ StackOverflow/StackOverflow.cpp
@@ -5,9 +5,7 @@
 #include <vector>
 
 
-StackOverflow::StackOverflow(): userIdCounter(1), postIdCounter(1),commentCounter(1){
-    
-
+StackOverflow::StackOverflow(): userIdCounter(1), postIdCounter(1),commentIdCounter(1){
 }
 
 
@@ -92,4 +90,115 @@ bool StackOverflow::votePost(const string& userId, const string& postId) {
     }
 
     return false;
+}
+
+bool StackOverflow::unvotePost(const string& userId, const string& postId) {
+
+    Post *post = findPost(postId);
+
+    if(!post) return false;
+
+    if(post->removeVote(userId)){
+        updateUsersReputation(post->getUserId(),-10);
+        return true;
+    }
+
+    return false;
+}
+
+bool StackOverflow::acceptAnswer(const string&userId, const string& answerId) {
+    Post* answer = findPost(answerId);
+
+    if(!answer || answer->getType()!= PostType::ANSWERS) return false;
+
+    answer->setAccepted(true);
+    updateUsersReputation(answer->getUserId(),15);
+    return true;
+
+}
+
+
+
+vector<Post*> StackOverflow::searchQuestions(const string& tag) const {
+    vector<Post*> results;
+
+    for(const auto& post : posts){
+        if(post->getType() == PostType::QUESTIONS) {
+            const auto& tags = post->getTags();
+            if( find(tags.begin(),tags.end(),tag)!=tags.end()) {
+                results.push_back(post);
+            }
+        }
+    }
+
+    return results;
+}
+
+void StackOverflow::displayUserProfile(const string& userId) const {
+    // Post* question = findPost(questionId);
+    User *user = findUser(userId);
+
+    if (!user) return;
+
+    user->displayInfo();
+
+    cout<<"\n Post by "<<user->getUsername()<<":" << endl;
+    for(const auto& post : posts){
+        if(post->getUserId() == userId){
+            post->displayInfo();
+        }
+    }
+
+}
+
+
+void StackOverflow::displayQuestion(const string& questionId) const {
+    Post* question = findPost(questionId);
+
+    if(!question || question->getType()!= PostType::QUESTIONS) return;
+    question->displayInfo();
+
+    cout<<"\n Answer:" << std::endl;
+
+    for(const auto& post : posts) {
+        if( post->getType() == PostType::ANSWERS) {
+            post->displayInfo();
+            cout<< " -------------------------------- " << std::endl;
+        }
+    }
+}
+
+
+User* StackOverflow::findUser(const string& userId) const {
+    auto it = find_if(users.begin(), users.end(), [userId](User* user) {
+        return user->getUserId() == userId;
+    });
+    return it != users.end() ? *it : nullptr;
+}
+
+Post* StackOverflow::findPost(const string& postId) const {
+    auto it = find_if(posts.begin(), posts.end(), [postId](Post* post) {
+        return post->getPostId() == postId;
+    });
+    return it!= posts.end()? *it : nullptr;
+}
+
+void StackOverflow::updateUsersReputation(const string& userId, int points) {
+    User* user = findUser(userId);
+    if(user) user->updateReputation(points);
+}
+
+
+string StackOverflow::generateUserId() {
+    return "U" +to_string(userIdCounter++);
+}
+
+
+string StackOverflow::generatePostId() {
+    return "P" + to_string(postIdCounter++);
+}
+
+
+string StackOverflow::generateCommentId() {
+    return "C" + to_string(commentIdCounter++);
 }
